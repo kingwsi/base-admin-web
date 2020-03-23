278d360d0ec9bc90d7f4088f54db667e3941ad84
kingwsi <35095609+kingwsi@users.noreply.github.com>
2020-03-23 09:43:08 +0800

    新增页面

---
+++ admin-web/src/views/permission/organization.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="app-container">
+    <el-row>
+      组织
+    </el-row>
+  </div>
+</template>
---
+++ admin-web/src/views/permission/user.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="app-container">
+    <el-row>
+      USER page
+    </el-row>
+  </div>
+</template>
