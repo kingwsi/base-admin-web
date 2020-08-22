9258aa06c480e623368c90a6b671e1d9f4db924a
kingwsi <35095609+kingwsi@users.noreply.github.com>
2020-08-22 15:21:11 +0800

    Update build-docker.sh

--- shell/build-docker.sh
+++ shell/build-docker.sh
@@ -1,5 +1,6 @@
 #docker 镜像/容器名字或者jar名字 这里都命名为这个
 SERVER_NAME=base-admin-web
+echo '---------准备构建${SERVER_NAME}---------'
 
 #容器id
 CID=$(docker ps | grep "$SERVER_NAME" | awk '{print $1}')
@@ -14,30 +15,34 @@ cp -r ../dist ./
 
 # 构建docker镜像
 if [ -n "$IID" ]; then
-        echo "删除旧$SERVER_NAME镜像，IID=$IID"
+        echo "删除$SERVER_NAME镜像，IID=$IID"
         docker rmi $IID
         echo "已删除$SERVER_NAME，重新构建镜像"
         docker build -t $SERVER_NAME .
 else
-        echo "不存在$SERVER_NAME镜像，开始构建镜像"
+        echo "开始构建$SERVER_NAME"
         docker build -t $SERVER_NAME .
-        echo "构建完成！即将运行$SERVER_NAME"
+        echo "$SERVER_NAME构建完成！准备启动"
 fi
 # 停掉原来的
 if [ -n "$CID" ]; then
         echo "正在停止$SERVER_NAME，CID=$CID"
         docker stop $CID
-        echo "已停止！"
+        echo "$SERVER_NAME已停止！"
 else
-        echo "容器 $SERVER_NAME 未运行"
+        echo "容器 $SERVER_NAME 未运行，即将启动..."
 fi
 
 # 运行docker容器
 docker run \
---name $SERVER_NAME\
 --rm \
--d\
--p 18092:80\
+--name $SERVER_NAME \
+-d \
+-p 18092:80 \
 $SERVER_NAME
 
-echo "$SERVER_NAME 已启动..."
+if [ $? -ne 0 ];then
+    echo "$SERVER_NAME 启动失败！"
+else
+    echo "$SERVER_NAME 已启动..."
+fi
