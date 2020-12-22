70eb77643db7525d8fc21cdc3a4405c25d409781
kingwsi <kingwsi@icloud.com>
2020-12-22 14:16:42 +0800

    获取userinfo以及登出问题

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,5 +1,5 @@
 import storage from 'store'
-import { login, logout } from '@/api/login'
+import { login } from '@/api/login'
 import { GetUserInfo } from '@/api/user'
 import { ACCESS_TOKEN } from '@/store/mutation-types'
 import { welcome } from '@/utils/util'
@@ -88,15 +88,10 @@ const user = {
     // 登出
     Logout ({ commit, state }) {
       return new Promise((resolve) => {
-        logout(state.token).then(() => {
-          resolve()
-        }).catch(() => {
-          resolve()
-        }).finally(() => {
-          commit('SET_TOKEN', '')
-          commit('SET_ROLES', [])
-          storage.remove(ACCESS_TOKEN)
-        })
+        commit('SET_TOKEN', '')
+        commit('SET_ROLES', [])
+        storage.remove(ACCESS_TOKEN)
+        resolve()
       })
     }
 
