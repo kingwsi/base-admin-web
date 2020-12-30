85637380bc6654786cfeba8442570b4908a448d9
kingwsi <kingwsi@icloud.com>
2020-12-30 16:51:00 +0800

    角色编辑优化

--- src/views/role/EditResource.vue
+++ src/views/role/EditResource.vue
@@ -3,27 +3,32 @@
     <a-spin :spinning="loading">
       <a-card :bordered="false">
         <a-row>
-          <a-form :form="form" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
-            <a-form-item v-show="roleId" label="主键ID">
-              <a-input v-decorator="['id']" disabled />
-            </a-form-item>
-            <a-form-item label="名称">
-              <a-input v-decorator="['name', { }]" />
-            </a-form-item>
-            <a-form-item label="描述">
-              <a-input v-decorator="['description', { }]" />
-            </a-form-item>
-            <a-form-item label="菜单权限">
+          <a-form-model
+            ref="form"
+            :model="model"
+            :rules="rules"
+            :label-col="{ span: 5 }"
+            :wrapper-col="{ span: 12 }">
+            <a-form-model-item v-show="roleId" label="ID">
+              <a-input v-model="model.id" disabled />
+            </a-form-model-item>
+            <a-form-model-item label="角色名称">
+              <a-input v-model="model.name" />
+            </a-form-model-item>
+            <a-form-model-item label="角色描述">
+              <a-input v-model="model.description" />
+            </a-form-model-item>
+            <a-form-model-item label="菜单权限">
               <a-row>
                 <a-col :span="10">
                   <a-tree
-                    :checkedKeys="menuSelectedKeys"
-                    :default-expand-all="true"
+                    v-model="menuSelectedKeys"
+                    :checkStrictly="true"
                     checkable
                     show-line
+                    :defaultExpandAll="true"
                     :tree-data="treeData.menuTree"
                     :replaceFields="treeFields"
-                    @check="onMenuCheck"
                   >
                     <a-icon slot="switcherIcon" type="down" />
                   </a-tree>
@@ -35,12 +40,11 @@
                     :checkedKeys="apiSelectedKeys"
                     :tree-data="treeData.apiTree"
                     :replaceFields="treeFields"
-                    @check="onApiCheck"
                   />
                 </a-col>
               </a-row>
-            </a-form-item>
-          </a-form>
+            </a-form-model-item>
+          </a-form-model>
         </a-row>
       </a-card>
     </a-spin>
@@ -54,15 +58,12 @@
   </page-header-wrapper>
 </template>
 <script>
-import pick from 'lodash.pick'
 import { GetAllResources } from '@/api/resource/index'
 import { GetRoleById, UpdateById, CreateRole } from '@/api/role'
 import { listToTree } from '@/utils/util'
 import { Tree } from 'ant-design-vue'
 import FooterToolBar from '@/components/FooterToolbar'
-
 // 表单字段
-const fields = ['id', 'name', 'description']
 export default {
     name: 'EditResource',
     components: {
@@ -72,8 +73,7 @@ export default {
     data () {
       return {
         roleId: null,
-        form: this.$form.createForm(this),
-        roleInfo: null,
+        model: {},
         loading: false,
         resourceList: [],
         menuSelectedKeys: [],
@@ -87,7 +87,10 @@ export default {
           title: 'name',
           value: 'id'
         },
-        parentIds: []
+        parentIds: [],
+        rules: {
+          name: [{ required: true, message: '请输入角色名称', trigger: 'change' }]
+        }
       }
     },
     methods: {
@@ -106,21 +109,18 @@ export default {
         this.loading = true
         this.roleId = this.$route.params.id
           GetRoleById(this.roleId).then(resp => {
-            this.roleInfo = resp.data
-            // 为form表单填充值
-            this.form.setFieldsValue(pick(resp.data, fields))
+            this.model = resp.data
             // 选中复选框处理
             if (resp.data && resp.data.resourceList) {
               this.menuSelectedKeys = []
               this.apiSelectedKeys = []
-              this.roleInfo.resourceList.forEach(element => {
+              resp.data.resourceList.forEach(element => {
                 if (element.type === 'MENU') {
                   this.menuSelectedKeys.push(element.id)
                 } else {
                   this.apiSelectedKeys.push(element.id)
                 }
               })
-              this.handleTreeChecked()
           }
           this.loading = false
         })
@@ -134,12 +134,15 @@ export default {
       },
       handleOk () {
         this.loading = true
-        this.form.validateFields((errors, formData) => {
-          if (!errors) {
-            formData.resourceIdList = this.menuSelectedKeys.concat(this.apiSelectedKeys)
-            if (formData.id) {
+        const form = this.$refs.form
+        form.validate(valid => {
+          if (valid) {
+            const checkedArrays = (this.menuSelectedKeys.checked || this.menuSelectedKeys).concat(this.apiSelectedKeys.checked || this.apiSelectedKeys)
+            console.log(checkedArrays)
+            this.model.resourceIdList = checkedArrays
+            if (this.model.id) {
               // 修改 e.g.
-              UpdateById(formData).then(res => {
+              UpdateById(this.model).then(res => {
                 this.loading = false
                 this.$message.info('修改成功')
                 this.$router.push({
@@ -152,7 +155,7 @@ export default {
               })
             } else {
               // 新增
-              CreateRole(formData).then(res => {
+              CreateRole(this.model).then(res => {
                 this.loading = false
                 this.$message.info('新增成功')
                 this.$router.push({
